4002ceddd590b5afedf4e94218dd462e51958f30
carolina.silvag <carolinasguzman@gmail.com>
2018-05-13 23:48:19 -0400

    algo

--- app/controller/postController.js
+++ app/controller/postController.js
@@ -1,13 +1,20 @@
 exports.test = function (req, res) {
-    var obj = {
-        'nombre': 'Pedrito',
-        'edad': '30',
-        'pais': 'Chile',
-        'ciudad': 'Valparaiso',
-        'color': 'Verde'
-    }
 
-    obj['comuna'] = 'Viña del Mar';
-    
-    res.status(200).send(obj);
+  var js2xmlparser = require("js2xmlparser");
+   var k = req.body.key;
+   var obj = {
+     'nombre': 'pedrito',
+     'edad': '30',
+     'color': 'negro',
+     'pais': 'chile',
+     'ciudad': 'valparaiso'
+   }
+   obj['keySalida'] = k;
+
+   var xml = js2xmlparser.parse("test", obj);
+
+   console.log(xml);
+
+   res.status(200).send(xml);
 }
+    
